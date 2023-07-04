a68941522c76bb91b7a169658e6d60a9d01ab099
hemantkishore7 <hemantkishore98@gmail.com>
2023-07-04 14:00:06 +0530

    update

--- Day-4-Task/script.js
+++ Day-4-Task/script.js
@@ -1,17 +1,37 @@
-var request=new XMLHttpRequest();
+var request = new XMLHttpRequest();
 
-request.open("GET","https://restcountries.com/v3.1/all");
+request.open("GET", "https://restcountries.com/v3.1/all");
 
 request.send();
 
-request.onload = function() {
-    let result = JSON.parse(request.response)
-    console.log(result);
-    for(var i = 0; i<=result.length;i++){
-       
-        console.log(result[i].flag);
+request.onload = function () {
+  let result = JSON.parse(request.response);
+  console.log(result);
+  for (var i = 0; i <= result.length; i++) {
 
+    //Print all country flags
+    console.log("Flag of"+ result[i].name.common+ ": " +result[i].flag);
+
+    //Print all countries name, regions, sub-regions & populations
+    console.log(
+      result[i].name.common +
+        "-" +
+        result[i].region +
+        "-" +
+        result[i].subregion +
+        "-" +
+        result[i].population
+    );
+  }
+};
+
+let obj1 = { 
+    name : "person1",
+    age : 5
     }
-
-}
-
+    let obj2 = {
+    age : 5,
+    name : "person1"
+    }
+    
+    console.log(JSON.stringify(obj1)===JSON.stringify(obj2));
